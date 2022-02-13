1b3d1e1a44415c64cc2974b7d99c90db3a1e778a
Nakul Sharma <74112608+nakulsharma9@users.noreply.github.com>
2022-02-13 19:40:42 +0530

    added the nav and its working so far

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,19 @@
-$(".borgir").on("click", function () {
-  $(".mobile-nav").addClass("open");
-});
-$(".borgirclose").on("click", function () {
-  $(".mobile-nav").removeClass("open");
-});
-$(".mobile-nav ul li a").on("click", function () {
-  $(".mobile-nav").removeClass("open");
-});
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
