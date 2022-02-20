729ef5071b727e0250557b99e81f49e2ac23e46f
raghav0477 <sharmaraghav0412@gmail.com>
2022-02-20 14:26:02 +0530

    added js for hidding nav on scroll

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("mobilenav").style.top = "0";
+  } else {
+    document.getElementById("mobilenav").style.top = "-100%";
+  }
+  prevScrollpos = currentScrollPos;
+}
