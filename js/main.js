1885010afb6ef8c368c7cf2249f641f263225883
raghav0477 <sharmaraghav0412@gmail.com>
2022-02-23 23:30:59 +0530

    done a bit of responsiveness changes

--- js/main.js
+++ js/main.js
@@ -8,3 +8,22 @@ window.onscroll = function () {
   }
   prevScrollpos = currentScrollPos;
 };
+
+const toggle = document.getElementById("toggle");
+const navbar = document.getElementById("navbar");
+const header = document.getElementById("header");
+
+document.onclick = function (e) {
+  if (
+    e.target.id !== "header" &&
+    e.target.id !== "toggle" &&
+    e.target.id !== "navbar"
+  ) {
+    toggle.classList.remove("active");
+    navbar.classList.remove("active");
+  }
+};
+toggle.onclick = function () {
+  toggle.classList.toggle("active");
+  navbar.classList.toggle("active");
+};
