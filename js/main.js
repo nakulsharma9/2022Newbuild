9e547465863a91bf044720d7e86f69682628420d
raghav0477 <sharmaraghav0412@gmail.com>
2022-02-25 21:10:58 +0530

    added custom cursor

--- js/main.js
+++ js/main.js
@@ -27,3 +27,32 @@ toggle.onclick = function () {
   toggle.classList.toggle("active");
   navbar.classList.toggle("active");
 };
+
+let innerCursor = document.querySelector(".innercursor");
+let outerCursor = document.querySelector(".outercursor");
+
+document.addEventListener("mousemove", moveCursor);
+
+function moveCursor(e) {
+    let x = e.clientX;
+    let y = e.clientY;
+    innerCursor.style.left = `${x}px`;
+    innerCursor.style.top = `${y}px`;
+    outerCursor.style.left = `${x}px`;
+    outerCursor.style.top = `${y}px`;
+
+    // console.log(e);
+}
+
+let links = Array.from(document.querySelectorAll("a"));
+
+console.log(links);
+
+links.forEach((link) => {
+    link.addEventListener("mouseover", () => {
+        innerCursor.classList.add("grow");
+    });
+    link.addEventListener("mouseleave", () => {
+        innerCursor.classList.remove("grow");
+    });
+});
